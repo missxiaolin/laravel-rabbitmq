0221c84662048885bf6f0ba6ccf907b4f0f5ed09
xiaolin <462441355@qq.com>
2018-08-03 18:02:35 +0800

    * 配置测试

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -16,4 +16,12 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testRabbitmq()
+    {
+        $config = config('rabbitmq');
+        $this->assertEquals($config['host'], env('RABBITMQ_HOST'));
+        $this->assertEquals($config['port'], env('RABBITMQ_PORT'));
+        $this->assertEquals($config['pass'], env('RABBITMQ_PASS'));
+    }
 }
